24aa95a7962572e9ef6a6f5de037dea0dcbbd36c
Derock <derock@derock.dev>
2023-09-17 20:26:02 -0400

    wip: auth

--- apps/api/src/controllers/auth/index.ts
+++ apps/api/src/controllers/auth/index.ts
@@ -1,9 +1,33 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import type { BaseElysiaContext } from "../..";
+import { db } from "../../database";
 
 export const authController = new Elysia<"/auth", BaseElysiaContext>({
   prefix: "/auth",
-}).get("/cookie", (...args) => {
-  console.log(args);
-  return "cookie";
-});
+})
+  .post(
+    "/login",
+    async (ctx) => {
+      // grab the user from the db and check the hashed password
+      const user = await db.query.user.findFirst({
+        where: (user, { eq }) => eq(user.id, ctx.body.username),
+      });
+
+      if (!user) {
+        ctx.set.status = 401;
+        return { error: "Invalid username or password" };
+      }
+
+      // check the password
+    },
+    {
+      body: t.Object({
+        username: t.String(),
+        password: t.String(),
+      }),
+    }
+  )
+  .get("/cookie", (...args) => {
+    console.log(args);
+    return "cookie";
+  });

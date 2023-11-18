a9cee9e4d25c2a81aba85fa71d3776306471b05d
Derock <derock@derock.dev>
2023-11-18 13:20:19 -0500

    feat: table to show containers

--- src/app/(dashboard)/_components/Project.tsx
+++ src/app/(dashboard)/_components/Project.tsx
@@ -1,8 +1,106 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { FaServer } from "react-icons/fa6";
+import { FaGear } from "react-icons/fa6";
 import { Service } from "./Service";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { Button } from "~/components/ui/button";
+import { DropdownMenuIcon } from "@radix-ui/react-icons";
+
+type Container = {
+  id: string;
+  name: string;
+  friendlyName: string;
+  status: "HEALTHY" | "STARTING" | "UNHEALTHY";
+  type: string;
+};
+
+const columns: ColumnDef<Container>[] = [
+  {
+    accessorKey: "friendlyName",
+    header: "Name",
+  },
+
+  {
+    accessorKey: "type",
+    header: "Type",
+  },
+
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="float-right mr-0 h-8 w-8 p-0">
+              <span className="sr-only">Actions</span>
+              <FaGear />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>test</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
 
 export function Project() {
+  const table = useReactTable({
+    data: [
+      {
+        id: "1",
+        name: "db",
+        friendlyName: "Database",
+        status: "HEALTHY",
+        type: "DATABASE",
+      },
+      {
+        id: "2",
+        name: "plausible",
+        friendlyName: "Plausible",
+        status: "HEALTHY",
+        type: "APP",
+      },
+      {
+        id: "3",
+        name: "clickhouse",
+        friendlyName: "Clickhouse",
+        status: "HEALTHY",
+        type: "DATABASE",
+      },
+    ] as Container[],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <Card>
       {/* 
@@ -43,11 +141,49 @@ export function Project() {
       </CardHeader>
 
       <CardContent>
-        <div className="flex flex-row flex-wrap justify-center gap-4">
-          <Service />
-          <Service />
-          <Service />
-        </div>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="text-center">
+                  <p className="text-muted-foreground">No services found</p>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
       </CardContent>
     </Card>
   );

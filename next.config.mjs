8cab5e54ce7ec27077cf047e023dfe4b02a1b341
Derock <derock@derock.dev>
2024-06-13 15:37:24 -0400

    fix webpack build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,38 @@
+/* eslint-disable */
+// @ts-nocheck
+
 /** @type {import("next").NextConfig} */
 const config = {
   eslint: {
     // will fix linting closer to a stable release
     ignoreDuringBuilds: true,
+  },
+
+  experimental: {
+    esmExternals: "loose",
+  },
+
+  /**
+   */
+  webpack: (config, { isServer }) => {
+    // Exclude .node files from bundling
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "node-loader",
+    });
+
+    // Ensure webpack doesn't try to bundle native modules
+    config.externals = config.externals || [];
+    if (isServer) {
+      config.externals.push(({ request }, callback) => {
+        if (/sshcrypto\.node$/.test(request)) {
+          return callback(null, `commonjs ${request}`);
+        }
+        callback();
+      });
+    }
+
+    return config;
   },
 };
 
